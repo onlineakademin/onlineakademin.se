839fb72870cf06fc2730ba12c73034db7eeb4446
Victor Andrée <hej@victorandree.se>
2015-05-25 18:29:28 +0200

    Hit video site directly

--- app/models/node.js
+++ app/models/node.js
@@ -33,23 +33,4 @@ let Node =  DS.Model.extend({
   }.property('type')
 });
 
-Node.reopenClass({
-  FIXTURES: [
-    { id: 'step-1', name: "Steg 1", ord: 1, type: "section", children: [ "step-1/ch-1", "step-1/ch-2", "step-1/ch-3", "step-1/ch-4" ], parent: null },
-    { id: 'step-2', name: "Steg 2", ord: 2, type: "section", children: [], parent: null },
-
-    { id: "step-1/ch-1", name: "Algebra och linjära modeller", ord: 1, type: "section", children: [ "step-1/ch-1/sec-1", "step-1/ch-1/sec-2", "step-1/ch-1/sec-3" ], parent: "step-1" },
-    { id: "step-1/ch-2", name: "Algebra och icke-linjära modeller", ord: 2, type: "section", children: [], parent: "step-1" },
-    { id: "step-1/ch-3", name: "Geometri", ord: 3, type: "section", children: [], parent: "step-1"  },
-    { id: "step-1/ch-4", name: "Statistik", ord: 4, type: "section", children: [], parent: "step-1" },
-
-    { id: "step-1/ch-1/sec-1", name: "Repetition av algebra och funktioner", ord: 1, type: "section", children: [ "step-1/ch-1/sec-1/vid-1", "step-1/ch-1/sec-1/vid-2" ], parent: "step-1/ch-1" },
-    { id: "step-1/ch-1/sec-2", name: "Räta linjens ekvation", ord: 2, type: "section", children: [], parent: "step-1/ch-1" },
-    { id: "step-1/ch-1/sec-3", name: "Linjära ekvationssystem", ord: 3, type: "section", children: [], parent: "step-1/ch-1" },
-
-    { id: "step-1/ch-1/sec-1/vid-1", name: "Descartes och Kartesiska Koordinater", ord: 1, type: "video", children: [], parent: "step-1/ch-1/sec-1", content: 'zpOULjyy-n8' },
-    { id: "step-1/ch-1/sec-1/vid-2", name: "Att rita grafer med koordinater", ord: 2, type: "video", children: [], parent: "step-1/ch-1/sec-1", content: 'kI9nFOzz1t8' }
-  ]
-});
-
 export default Node;
--- app/routes/nodes/node.js
+++ app/routes/nodes/node.js
@@ -5,6 +5,17 @@ export default Ember.Route.extend({
     return this.store.find('node', params.wildcard);
   },
 
+  // redirect to first child for level 1 nodes
+  // todo fix this properly
+  afterModel: function(node, transition) {
+    if (node.get('level') == 0) {
+      return this.transitionTo('nodes.node', node.get('id') + '/1/1');
+    }
+    if (node.get('level') == 1) {
+      return this.transitionTo('nodes.node', node.get('id') + '/1');
+    }    
+  },
+
   renderTemplate: function(controller, model) {
     var templateName;
 
