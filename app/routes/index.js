c743478a5efb8668c5ace5134b7cab447618fac5
Victor Andrée <hej@victorandree.se>
2015-07-22 11:10:32 +0200

    Always scroll to top when changing route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   activate: function() {
-    this._super();
-    window.scrollTo(0, 0);
-
     Ember.$("body").addClass('page-index');
     Ember.$(".site-header").removeClass('unmelt');
   },
