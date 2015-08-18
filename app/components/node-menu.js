99612b6ca4b9cab38578341982fd6dac76e5fb38
Victor Andrée <hej@victorandree.se>
2015-08-19 00:29:02 +0200

    Update ol.steps to handle line breaks correctly
    Uses a somewhat more robust `display: table-cell` solution to ensure
    that the two "columns" will never be in the way of each other. I guess
    that breaks backwards compatibility with some browsers, but IE8 is not
    supported anyway.

--- app/components/node-menu.js
+++ app/components/node-menu.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     toggleExpand: function(node) {
-     this.$('li[data-node-id="'+node.get('id')+'"]').toggleClass('not-expanded');
+     this.$('li[data-node-id="'+node.get('id')+'"]').toggleClass('expanded');
     }
   },
 });
