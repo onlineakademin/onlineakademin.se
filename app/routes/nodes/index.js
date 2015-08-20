9255882327a9e80b0f9e6b28bc1042ba48b3e581
Victor Andrée <victor.andree@izettle.com>
2015-08-20 17:52:21 +0200

    Clear up some deprectation warnings for Ember 2.0
    Remaining issue is with using Ember.View, which we need to get rid off
    somehow, and registration of helpers which comes from Ember itself.

--- app/routes/nodes/index.js
+++ app/routes/nodes/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('node', { parent: null });
+    return this.store.query('node', { parent: null });
   }
 });
